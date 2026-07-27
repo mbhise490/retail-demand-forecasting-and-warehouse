2de83412357b4d53bbd2bbe1e2f77092aa94ee1c
Mahesh Bhise <mbhise490@gmail.com>
2026-07-27 12:15:46 +0530

    Update and rename Prophet_Forecast_View.sql to time_seriesView.sql

---
+++ scripts/sql_views/time_seriesView.sql
@@ -0,0 +1,48 @@
+/*
+==========================================================
+Create Time Series Analysis View
+==========================================================
+Purpose:
+- Prepare daily data for time series analysis
+- Support EDA and feature engineering
+- Ready for Prophet, ARIMA, LSTM and XGBoost
+==========================================================
+*/
+
+IF OBJECT_ID('gold.vw_time_series','V') IS NOT NULL
+DROP VIEW gold.vw_time_series;
+GO
+
+CREATE VIEW gold.vw_time_series
+AS
+
+SELECT
+
+    d.Full_Date AS SalesDate,
+    SUM(f.SalesAmount) AS DailySales,
+    SUM(f.Quantity) AS TotalQuantity,
+    COUNT(DISTINCT f.InvoiceNo) AS TotalOrders,
+    COUNT(DISTINCT c.CustomerID) AS TotalCustomers,
+    AVG(f.UnitPrice) AS AvgUnitPrice,
+    -- Calendar features
+    YEAR(d.Full_Date) AS Year,
+    MONTH(d.Full_Date) AS Month,
+    DATENAME(MONTH, d.Full_Date) AS MonthName,
+    DAY(d.Full_Date) AS Day,
+    DATENAME(WEEKDAY, d.Full_Date) AS Weekday,
+    DATEPART(WEEK, d.Full_Date) AS WeekNumber,
+    DATEPART(QUARTER, d.Full_Date) AS Quarter,
+    CASE
+        WHEN DATENAME(WEEKDAY, d.Full_Date) IN ('Saturday','Sunday')
+        THEN 1
+        ELSE 0
+    END AS IsWeekend
+
+FROM gold.fact_sales f
+INNER JOIN gold.dim_date d
+    ON f.Date_Key = d.Date_Key
+INNER JOIN gold.dim_customer c
+    ON f.Customer_Key = c.Customer_Key
+GROUP BY
+    d.Full_Date;
+GO
